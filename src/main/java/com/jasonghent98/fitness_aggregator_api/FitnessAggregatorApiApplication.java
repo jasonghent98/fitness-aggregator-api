aa217663d6ac119c9b34092d38c4c401a539849c
Jason Ghent <jasonthomasghent@gmail.com>
2025-07-22 20:26:09 -0600

    initial spring boot project scaffold

---
+++ src/main/java/com/jasonghent98/fitness_aggregator_api/FitnessAggregatorApiApplication.java
@@ -0,0 +1,13 @@
+package com.jasonghent98.fitness_aggregator_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FitnessAggregatorApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FitnessAggregatorApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/jasonghent98/fitness_aggregator_api/FitnessAggregatorApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jasonghent98.fitness_aggregator_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FitnessAggregatorApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
