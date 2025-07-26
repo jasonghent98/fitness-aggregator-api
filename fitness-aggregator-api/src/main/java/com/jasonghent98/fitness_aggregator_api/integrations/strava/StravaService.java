dedc1814a10137e049a568c306b28a7c2807ec60
Jason Ghent <jasonthomasghent@gmail.com>
2025-07-26 12:33:04 -0600

    strava service logic in progress

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
@@ -1,9 +1,9 @@
-/*
+
 package com.jasonghent98.fitness_aggregator_api.integrations.strava;
 
-import io.swagger.client.ApiClient;
 import io.swagger.client.model.SummaryActivity;
 import io.swagger.client.api.ActivitiesApi;
+import io.swagger.client.ApiClient;
 
 import org.springframework.stereotype.Service;
 import java.util.List;
@@ -11,17 +11,17 @@ import java.util.List;
 @Service
 public class StravaService {
 
-    public static List<SummaryActivity> getActivities(String accessToken) {
+    public List<SummaryActivity> getActivities(String accessToken) {
         io.swagger.client.ApiClient client = new ApiClient();
-        client.setAccessToken(accessToken);  // Inject athlete's token dynamically
+        client.setAccessToken(accessToken);
 
         ActivitiesApi activitiesApi = new ActivitiesApi(client);
 
         try {
-            return activitiesApi.getLoggedInAthleteActivities(null, null, 1, 10);
+            return activitiesApi.getLoggedInAthleteActivities(null, null, 1,30);
         } catch (Exception e) {
             throw new RuntimeException("Failed to fetch Strava activities", e);
         }
     }
+
 }
-*/
