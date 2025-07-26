7c9af43ac97c719e1d920fb6c5acca9e77ad5eea
Jason Ghent <jasonthomasghent@gmail.com>
2025-07-26 10:56:39 -0600

    health check cleanup

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/HealthCheckController.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/HealthCheckController.java
@@ -1,25 +1,14 @@
 package com.jasonghent98.fitness_aggregator_api.controller;
-import com.jasonghent98.fitness_aggregator_api.integrations.strava.StravaService;
-import io.swagger.client.model.SummaryActivity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-import java.util.List;
+import org.springframework.web.bind.annotation.*;
+
 
 @RestController
-@RequestMapping("/api/health")
+@RequestMapping("/api")
 public class HealthCheckController {
-    @GetMapping
-    public String healthCheck() {
-        return "Backend running!";
+
+    @GetMapping("/config")
+    public void getHealthCheck(){
+        /*ping the db server and return an OK*/
     }
-    @GetMapping("/db")
-    public String checkDatabase() {
-        return "DB is running!";
-    }
-    @GetMapping("/strava")
-    public void getStravaUser() {
-        List<SummaryActivity> summaryActivities = StravaService.getActivities("5fb9d18611c56d19015e8249e1f5980370506ecc");
-        System.out.println(summaryActivities);
-    }
+
 }
