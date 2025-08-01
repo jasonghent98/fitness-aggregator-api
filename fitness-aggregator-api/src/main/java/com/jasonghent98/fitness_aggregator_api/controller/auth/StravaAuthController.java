dc699944e1c401be9866f4fa757692f55f1423b3
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-01 00:21:36 -0600

    strava auth token create + update cases for new and existing users; storing token data in strava_users

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/auth/StravaAuthController.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/auth/StravaAuthController.java
@@ -2,6 +2,7 @@ package com.jasonghent98.fitness_aggregator_api.controller.auth;
 
 import com.jasonghent98.fitness_aggregator_api.config.services.StravaConfig;
 import com.jasonghent98.fitness_aggregator_api.dto.strava.StravaAuthTokenResponse;
+import com.jasonghent98.fitness_aggregator_api.model.User;
 import com.jasonghent98.fitness_aggregator_api.model.strava.StravaUser;
 import com.jasonghent98.fitness_aggregator_api.repository.UserRepository;
 import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaUserRepository;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.net.URI;
+import java.time.Instant;
 import java.util.Optional;
 
 @RestController
@@ -73,14 +75,49 @@ public class StravaAuthController {
             );
 
             ResponseEntity<StravaAuthTokenResponse> tokenData = response;
-            if (tokenData.getBody() != null) {
-                System.out.println(" FROM AUTH!! " + tokenData.getBody().athlete.id);
+
+            if (tokenData == null) {
+                return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body("Empty response from Strava Authentication");
             }
 
-            // check if the user exists in the strava_users table
+            // unpack the variables from the strava oauth response
+            Long stravaAthleteId = tokenData.getBody().athlete.id;
+            String stravaUsername = tokenData.getBody().athlete.username;
+            String stravaFirstName = tokenData.getBody().athlete.firstName;
+            String stravaLastName = tokenData.getBody().athlete.lastName;
+            Instant now = Instant.now();
 
-            // Normally you'd parse the response into an object and persist it.
-            return ResponseEntity.ok(response.getBody().toString());
+            // get the associated strava user if exists
+            Optional<StravaUser> existingStravaUserOpt = stravaUserRepo.findByStravaAthleteId(stravaAthleteId);
+
+            // if strava user exists, just update the token; else, create new entry in both users and strava_users tables
+            if (existingStravaUserOpt.isPresent()) {
+
+                StravaUser existing = existingStravaUserOpt.get();
+                existing.setAccessToken(tokenData.getBody().accessToken);
+                existing.setRefreshToken(tokenData.getBody().refreshToken);
+                existing.setExpiresAt(Instant.ofEpochSecond(tokenData.getBody().expiresAt));
+                stravaUserRepo.save(existing);
+
+            } else {
+
+                // Create User
+                User newUser = new User();
+                newUser.setFullName(stravaFirstName + " " + stravaLastName);
+                newUser.setUsername(stravaUsername);
+                userRepo.save(newUser);
+
+                // Create StravaUser
+                StravaUser newStravaUser = new StravaUser();
+                newStravaUser.setUser(newUser);
+                newStravaUser.setStravaAthleteId(stravaAthleteId);
+                newStravaUser.setAccessToken(tokenData.getBody().accessToken);
+                newStravaUser.setRefreshToken(tokenData.getBody().refreshToken);
+                newStravaUser.setExpiresAt(Instant.ofEpochSecond(tokenData.getBody().expiresAt));
+                stravaUserRepo.save(newStravaUser);
+            }
+
+            return ResponseEntity.ok("Strava authentication successful");
 
         } catch (Exception e) {
             e.printStackTrace();
