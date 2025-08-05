d0ac57712086cbdd70cbcb88bef3061f0dc36373
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-05 14:13:04 -0600

    scaffolding for the StravaService.getAthleteStats method

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
@@ -2,12 +2,15 @@
 package com.jasonghent98.fitness_aggregator_api.integrations.strava;
 
 import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaUserRepository;
+import io.swagger.client.model.ActivityStats;
 import io.swagger.client.model.SummaryActivity;
 import io.swagger.client.api.ActivitiesApi;
 import io.swagger.client.api.AthletesApi;
 import io.swagger.client.ApiClient;
 
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
 import java.util.List;
 
 @Service
@@ -18,11 +21,11 @@ public class StravaService {
         this.stravaUserRepo = stravaUserRepo;
     }
 
-
+    // returns all activities for a given user
     public List<SummaryActivity> getActivities(String userAccessToken) {
+        // set up api client
         ApiClient client = new ApiClient();
         client.setAccessToken(userAccessToken);
-
         ActivitiesApi activitiesApi = new ActivitiesApi(client);
 
         try {
@@ -31,23 +34,34 @@ public class StravaService {
             throw new RuntimeException("Failed to fetch Strava activities", e);
         }
     }
-/*
-    public List<SummaryActivity> getAthleteStats(String userAccessToken) {
+
+    // returns athlete stats and summaries (swim, bike, run)
+    public List<ActivityStats> getAthleteStats(String userAccessToken) {
+        // set up api client
         ApiClient client = new ApiClient();
         client.setAccessToken(userAccessToken);
-
         AthletesApi athletesApi = new AthletesApi(client);
 
-        try {
-            // get the corresponding strava_athlete_id based on userid
-            stravaUserRepo.findByUserId();
 
-            return athletesApi.getStats(88624072);
+        // Step 2: Get user id from userContext
+
+        // step 3: get user athlete id from userid using stravaUserRepo
+
+        // step 4: make call to athletesApi.getStats with user athlete id
+
+
+        /*
+        try {
+
         } catch (Exception e) {
             throw new RuntimeException("Failed to fetch Strava athlete stats", e);
         }
-    }
 
- */
+         */
+
+        return Collections.emptyList(); // temporary placeholder
+
+
+    }
 
 }
