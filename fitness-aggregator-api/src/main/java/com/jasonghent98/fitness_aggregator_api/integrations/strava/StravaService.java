45186343074fed9cead88c04d1461d9ea4e3016a
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-05 13:55:00 -0600

    stravaUserRepo as a singleton in StravaService to make DB calls

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
@@ -1,8 +1,10 @@
 
 package com.jasonghent98.fitness_aggregator_api.integrations.strava;
 
+import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaUserRepository;
 import io.swagger.client.model.SummaryActivity;
 import io.swagger.client.api.ActivitiesApi;
+import io.swagger.client.api.AthletesApi;
 import io.swagger.client.ApiClient;
 
 import org.springframework.stereotype.Service;
@@ -10,9 +12,15 @@ import java.util.List;
 
 @Service
 public class StravaService {
+    StravaUserRepository stravaUserRepo;
+
+    StravaService(StravaUserRepository stravaUserRepo) {
+        this.stravaUserRepo = stravaUserRepo;
+    }
+
 
     public List<SummaryActivity> getActivities(String userAccessToken) {
-        io.swagger.client.ApiClient client = new ApiClient();
+        ApiClient client = new ApiClient();
         client.setAccessToken(userAccessToken);
 
         ActivitiesApi activitiesApi = new ActivitiesApi(client);
@@ -23,5 +31,23 @@ public class StravaService {
             throw new RuntimeException("Failed to fetch Strava activities", e);
         }
     }
+/*
+    public List<SummaryActivity> getAthleteStats(String userAccessToken) {
+        ApiClient client = new ApiClient();
+        client.setAccessToken(userAccessToken);
+
+        AthletesApi athletesApi = new AthletesApi(client);
+
+        try {
+            // get the corresponding strava_athlete_id based on userid
+            stravaUserRepo.findByUserId();
+
+            return athletesApi.getStats(88624072);
+        } catch (Exception e) {
+            throw new RuntimeException("Failed to fetch Strava athlete stats", e);
+        }
+    }
+
+ */
 
 }
