8ec156e52f3ef36f6e8e2b62676320a889acfdcc
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-07 13:41:57 -0600

    getStatsForUser method declaration + implementation cleanup

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/integrations/strava/StravaService.java
@@ -3,14 +3,11 @@ package com.jasonghent98.fitness_aggregator_api.integrations.strava;
 
 import com.jasonghent98.fitness_aggregator_api.context.UserContext;
 import com.jasonghent98.fitness_aggregator_api.model.strava.StravaActivity;
+import com.jasonghent98.fitness_aggregator_api.model.strava.StravaStats;
 import com.jasonghent98.fitness_aggregator_api.model.strava.StravaUser;
 import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaActivityRepository;
+import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaStatsRepository;
 import com.jasonghent98.fitness_aggregator_api.repository.strava.StravaUserRepository;
-import io.swagger.client.model.ActivityStats;
-import io.swagger.client.model.SummaryActivity;
-import io.swagger.client.api.ActivitiesApi;
-import io.swagger.client.api.AthletesApi;
-import io.swagger.client.ApiClient;
 
 import org.springframework.stereotype.Service;
 
@@ -20,12 +17,15 @@ import java.util.UUID;
 // All data is retrieved from DB directly (no API calls)
 @Service
 public class StravaService {
-    StravaUserRepository stravaUserRepo;
     StravaActivityRepository stravaActRepo;
+    StravaStatsRepository stravaStatsRepo;
 
-    StravaService(StravaUserRepository stravaUserRepo, StravaActivityRepository stravaActRepo) {
-        this.stravaUserRepo = stravaUserRepo;
+    StravaService(
+            StravaActivityRepository stravaActRepo,
+            StravaStatsRepository stravaStatsRepo
+            ) {
         this.stravaActRepo = stravaActRepo;
+        this.stravaStatsRepo = stravaStatsRepo;
     }
 
     // Returns all activities for a given user from our DB
@@ -34,27 +34,10 @@ public class StravaService {
         return stravaActRepo.findByUserIdOrderByStartDateDesc(userId);
     }
 
-    // returns athlete stats and summaries (swim, bike, run)
-    public ActivityStats getAthleteStats(String userAccessToken) {
-        // set up api client
-        ApiClient client = new ApiClient();
-        client.setAccessToken(userAccessToken);
-        AthletesApi athletesApi = new AthletesApi(client);
-
-
-        // Step 2: Get user id from userContext
+    // returns athlete stats and summaries (swim, bike, run) from our DB
+    public List<StravaStats> getStatsForUser() {
         UUID userId = UserContext.getUserId();
-
-        // step 3: get user athlete id from userid using stravaUserRepo
-        StravaUser stravaUser = stravaUserRepo.findByUserId(userId).orElseThrow(() -> new RuntimeException("Strava user not found for userId: " + userId));
-        Long stravaAthleteId = stravaUser.getStravaAthleteId();
-
-        // step 4: make call to athletesApi.getStats with user athlete id
-        try {
-            return athletesApi.getStats(stravaAthleteId);
-        } catch (Exception e) {
-            throw new RuntimeException("Failed to fetch Strava athlete stats", e);
-        }
+        return stravaStatsRepo.findByUserId(userId);
 
     }
 
