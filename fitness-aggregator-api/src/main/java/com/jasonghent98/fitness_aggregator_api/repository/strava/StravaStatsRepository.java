ab9c63784953d85e0b6c76dc75cce04ea0da9a4e
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-07 13:41:22 -0600

    stravastatsrepo created to interface with DB strava_stats table

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/repository/strava/StravaStatsRepository.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/repository/strava/StravaStatsRepository.java
@@ -2,9 +2,9 @@ package com.jasonghent98.fitness_aggregator_api.repository.strava;
 
 import com.jasonghent98.fitness_aggregator_api.model.strava.StravaStats;
 import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.Optional;
+import java.util.List;
 import java.util.UUID;
 
 public interface StravaStatsRepository extends JpaRepository<StravaStats, UUID> {
-    Optional<StravaStats> findByUserId(UUID userId);
+    List<StravaStats> findByUserId(UUID userId);
 }
