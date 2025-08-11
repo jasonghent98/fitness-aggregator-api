8b9b7ee62b993b7b9e2a3f6771fe71e82b0ea1a9
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-10 18:19:05 -0600

    fitbit config implemented for auth secret values

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/provider/fitbit/FitbitConfig.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/provider/fitbit/FitbitConfig.java
@@ -1,4 +1,20 @@
 package com.jasonghent98.fitness_aggregator_api.config.provider.fitbit;
 
+import lombok.Data;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+@Component
+@Data
 public class FitbitConfig {
+
+    @Value("${fitbit.CLIENT_ID}")
+    private String clientId;
+
+    @Value("${fitbit.CLIENT_SECRET}")
+    private String clientSecret;
+
+    @Value("${fitbit.REDIRECT_URL}")
+    private String redirectUrl;
+
 }
