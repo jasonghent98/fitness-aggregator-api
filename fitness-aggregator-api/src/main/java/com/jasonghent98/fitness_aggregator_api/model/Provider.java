3d7bf40dd1266d044dfcbe2aa605fee3dc6cd1b1
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-18 19:26:34 -0400

    provider model created for mapping our providers in backend

---
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/model/Provider.java
@@ -0,0 +1,21 @@
+package com.jasonghent98.fitness_aggregator_api.model;
+
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+// Provider.java
+@Entity
+@Table(name = "providers")
+@Data
+public class Provider {
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Short id;
+    @Column(nullable=false) private String displayName;
+    @Column(nullable=false) private String authType; // 'oauth2' | 'oauth1' | 'custom'
+    private String authorizeUrl;
+    private String tokenUrl;
+    private String defaultScopes;
+    private String docsUrl;
+    @Column(nullable=false) private boolean enabled = true;
+}
