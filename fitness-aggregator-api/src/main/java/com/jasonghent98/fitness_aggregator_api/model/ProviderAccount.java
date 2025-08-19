e6847ad09829e13f9cd7ee261511b0eeb4a7ef2e
Jason Ghent <jasonthomasghent@gmail.com>
2025-08-18 20:11:56 -0400

    provider account intoduced, will need to remove each <provider>_users table now

---
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/model/ProviderAccount.java
@@ -0,0 +1,59 @@
+package com.jasonghent98.fitness_aggregator_api.model;
+
+import jakarta.persistence.*;
+import lombok.Data;
+
+import java.time.Instant;
+import java.util.UUID;
+
+// central table for all providers (strava, fitbit, etc..)
+@Entity
+@Table(
+        name = "provider_accounts",
+        uniqueConstraints = {
+                @UniqueConstraint(name = "uq_provider_accounts_provider_ext",
+                        columnNames = {"provider_id","provider_user_id"}),
+                @UniqueConstraint(name = "uq_provider_accounts_user_provider",
+                        columnNames = {"user_id","provider_id"})
+        },
+        indexes = {
+                @Index(name = "idx_provider_accounts_user", columnList = "user_id"),
+                @Index(name = "idx_provider_accounts_provider", columnList = "provider_id")
+        }
+)
+@Data
+public class ProviderAccount {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO) // PK
+    private UUID id;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne(optional = false, fetch = FetchType.LAZY)
+    @JoinColumn(name = "provider_id", nullable = false)
+    private Provider provider;
+
+    @Column(nullable=false, name="provider_user_id") private String providerUserId; // string safe to catch all providers
+
+    @Column(name = "access_token", nullable = false)
+    private String accessToken;
+
+    @Column(name = "refresh_token", nullable = false)
+    private String refreshToken;
+
+    @Column(name = "expires_at", nullable = false)
+    private Instant expiresAt;
+
+
+    @Column(name="created_at", nullable=false)
+    private Instant createdAt;
+
+    @Column(name="updated_at", nullable=false)
+    private Instant updatedAt;
+
+    @PrePersist void onCreate() { Instant now = Instant.now(); createdAt = now; updatedAt = now; }
+    @PreUpdate void onUpdate() { updatedAt = Instant.now(); }
+
+}
