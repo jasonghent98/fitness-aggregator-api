c0e9016f5c8ca86ff1f99ba03bd94c8e4c836a6d
Jason Ghent <jasonthomasghent@gmail.com>
2025-09-09 05:32:08 -0600

    emailrepository file created

---
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/repository/EmailVerificationRepository.java
@@ -0,0 +1,13 @@
+package com.jasonghent98.fitness_aggregator_api.repository;
+
+import com.jasonghent98.fitness_aggregator_api.model.EmailVerification;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+import java.util.UUID;
+
+public interface EmailVerificationRepository extends JpaRepository<EmailVerification, UUID> {
+    // case-insensitive by virtue of citext on the column, but this reads clearer
+    Optional<EmailVerification> findByEmail(String email);
+    Optional<EmailVerification> findByAccessToken(String accessToken);
+}
