d57b80fcbdf897b0e02230e406406ad0d0b93dc5
Jason Ghent <jasonthomasghent@gmail.com>
2025-09-09 10:11:29 -0600

    updated run local dev script to start container if not

--- scripts/dev/run-db-local-shell.sh
+++ scripts/dev/run-db-local-shell.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
+# start it
+docker start actualize-db-dev
 # for testing purposes: runs the interactive docker container for psql (DEV)
-docker exec -it fitness-aggregator-api-postgres-1 psql -U actuserdev -d actualizedev
+docker exec -it actualize-db-dev psql -U actuserdev -d actualizedev
