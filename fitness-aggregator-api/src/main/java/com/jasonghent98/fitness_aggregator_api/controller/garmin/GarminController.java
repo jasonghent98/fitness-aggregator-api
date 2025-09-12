49de74df585cd53756c9e8ea63f0b0227ce8cef1
Jason Ghent <jasonthomasghent@gmail.com>
2025-09-11 19:21:55 -0600

    garmin controller injection

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/garmin/GarminController.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/controller/garmin/GarminController.java
@@ -1,5 +1,6 @@
 package com.jasonghent98.fitness_aggregator_api.controller.garmin;
 
+import com.jasonghent98.fitness_aggregator_api.service.garmin.GarminWebhookService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -8,19 +9,19 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/garmin")
-@RequiredArgsConstructor
 public class GarminController {
 
+    private final GarminWebhookService garminWebServ;
 
-    // WEBHOOKS //
-
-    private final GarminWebhookService garminWebhookService;
+    public GarminController(GarminWebhookService garminWebServ) {
+        this.garminWebServ = garminWebServ;
+    }
 
     @PostMapping
     public ResponseEntity<Void> receive(@RequestBody Map<String, Object> payload,
                                         @RequestHeader Map<String, String> headers) {
         // TODO: verify HMAC signature later using headers (X-...)
-        garminWebhookService.handleEvent(payload);
+        garminWebServ.handleEvent(payload);
         return ResponseEntity.noContent().build(); // 204 quick response
     }
 }
