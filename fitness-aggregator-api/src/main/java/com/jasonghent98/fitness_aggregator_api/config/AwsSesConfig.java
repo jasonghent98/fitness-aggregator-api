8d7fb97548909f960a1efda640e5e56f93e11b5b
Jason Ghent <jasonthomasghent@gmail.com>
2025-09-23 22:28:58 -0600

    send async emails method implemented for emailservice

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/AwsSesConfig.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/AwsSesConfig.java
@@ -1,4 +1,21 @@
 package com.jasonghent98.fitness_aggregator_api.config;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import software.amazon.awssdk.regions.Region;
+import software.amazon.awssdk.services.ses.SesClient;
+
+@Configuration
 public class AwsSesConfig {
+
+    @Value("${aws.ses.region}")
+    private String region;
+
+    @Bean
+    public SesClient sesClient() {
+        return SesClient.builder()
+                .region(Region.of(region))
+                .build();
+    }
 }
---
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/service/EmailService.java
@@ -0,0 +1,66 @@
+package com.jasonghent98.fitness_aggregator_api.service;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.scheduling.annotation.Async;
+import org.springframework.stereotype.Service;
+import software.amazon.awssdk.services.ses.SesClient;
+import software.amazon.awssdk.services.ses.model.*;
+
+
+@Service
+public class EmailService {
+
+    private final SesClient sesClient;
+
+    @Value("${aws.ses.source}")
+    private String sourceEmail;
+
+    @Value("${aws.ses.from-email}")
+    private String fromEmail;
+
+    public EmailService(SesClient sesClient) {
+        this.sesClient = sesClient;
+    }
+
+    /** Sends asynchronous emails related to app-related events (i.e. billing, authentication, etc.) */
+    @Async
+    public void sendEmail(String to, String subject, String bodyHtml, String bodyText) {
+
+        Destination destination = Destination.builder()
+                .toAddresses(to)
+                .build();
+
+        Content subjectContent = Content.builder()
+                .data(subject)
+                .charset("UTF-8")
+                .build();
+
+        Content textContent = Content.builder()
+                .data(bodyText)
+                .charset("UTF-8")
+                .build();
+
+        Content htmlContent = Content.builder()
+                .data(bodyHtml)
+                .charset("UTF-8")
+                .build();
+
+        Body body = Body.builder()
+                .text(textContent)
+                .html(htmlContent)
+                .build();
+
+        Message message = Message.builder()
+                .subject(subjectContent)
+                .body(body)
+                .build();
+
+        SendEmailRequest request = SendEmailRequest.builder()
+                .source(fromEmail)
+                .destination(destination)
+                .message(message)
+                .build();
+
+        sesClient.sendEmail(request);
+    }
+}
