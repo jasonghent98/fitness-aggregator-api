bc0937da51e282887fa1a81f8e3f75b7b40fb08c
Jason Ghent <jasonthomasghent@gmail.com>
2025-10-06 20:37:04 -0400

    stripe config

--- fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/StripeConfig.java
+++ fitness-aggregator-api/src/main/java/com/jasonghent98/fitness_aggregator_api/config/StripeConfig.java
@@ -1,4 +1,20 @@
 package com.jasonghent98.fitness_aggregator_api.config;
 
+import com.stripe.Stripe;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+
+import jakarta.annotation.PostConstruct;
+
+@Configuration
 public class StripeConfig {
+
+    @Value("${stripe.secret-key}")
+    private String secretKey;
+
+    @PostConstruct
+    public void init() {
+        // stripe-java uses a static apiKey by default
+        Stripe.apiKey = secretKey;
+    }
 }
